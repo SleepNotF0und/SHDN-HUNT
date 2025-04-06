d4eceb49614b81bb11b5e6a02fd87f85840c05e4
0xr3dhunt <51271019+SleepNotF0und@users.noreply.github.com>
2025-04-06 06:29:45 +0200

    Update SHDN-HUNT.py

--- SHDN-HUNT.py
+++ SHDN-HUNT.py
@@ -1,98 +1,104 @@
 import shodan
+import pyfiglet
+import sys
 
 
-
-#COLORS
-bcolors = {
-    'HEADER' : '\033[95m',
-    'OKBLUE' : '\033[94m',
-    'OKCYAN' : '\033[96m',
-    'OKGREEN' : '\033[92m',
-    'WARNING' : '\033[93m',
-    'FAIL' : '\033[91m',
-    'ENDC' : '\033[0m',
-    'BOLD' : '\033[1m',
-    'UNDERLINE' : '\033[4m'
-}
-
-
+# BANNER
 #BANNER
-print("\n\n      ___           ___           ___           ___  ")
-print("     /\  \         /\__\         /\  \         /\__\     ")
-print("    /::\  \       /:/  /        /::\  \       /::|  |    ")
-print("   /:/\ \  \     /:/__/        /:/\:\  \     /:|:|  |    ")
-print("  _\:\~\ \  \   /::\  \ ___   /:/  \:\__\   /:/|:|  |__  ")
-print(" /\ \:\ \ \__\ /:/\:\  /\__\ /:/__/ \:|__| /:/ |:| /\__\ ")
-print(" \:\ \:\ \/__/ \/__\:\/:/  / \:\  \ /:/  / \/__|:|/:/  / ")
-print("  \:\ \:\__\        \::/  /   \:\  /:/  /      |:/:/  /  ")
-print("   \:\/:/  /        /:/  /     \:\/:/  /       |::/  /   ")
-print("    \::/  /        /:/  /       \::/__/        /:/  /    ")
-print("     \/__/         \/__/         ~~            \/__/     ")
-print("      ___           ___           ___           ___      ")
-print("     /\__\         /\__\         /\__\         /\  \     ")
-print("    /:/  /        /:/  /        /::|  |        \:\  \    ")
-print("   /:/__/        /:/  /        /:|:|  |         \:\  \   ")
-print("  /::\  \ ___   /:/  /  ___   /:/|:|  |__       /::\  \  ")
-print(" /:/\:\  /\__\ /:/__/  /\__\ /:/ |:| /\__\     /:/\:\__\ ")
-print(" \/__\:\/:/  / \:\  \ /:/  / \/__|:|/:/  /    /:/  \/__/ ")
-print("      \::/  /   \:\  /:/  /      |:/:/  /    /:/  /      ")
-print("      /:/  /     \:\/:/  /       |::/  /     \/__/       ")
-print("     /:/  /       \::/  /        /:/  /                  ")
-print("     \/__/         \/__/         \/__/                   ")
-print(bcolors['BOLD'] +"Coded By Hazem Yasser | hackerone.com/0xr3dhunt\n\n" + bcolors['ENDC'])
+print("         _______         ")
+print("     .,-'\_____/`-.      ")
+print("     /`..'\ _ /`.,'\     ")
+print("    /  /`.,' `.,'\  \    ")
+print("   /__/__/  O  \__\__\   ")
+print("   \  \  \     /  /  /   ")
+print("    \  \,'`._,'`./  /    ")
+print("     \,'`./___\,'`./     ")
+print("     '`-./_____\,-'`     ")
+print("                         ")
+print("Coded By Hazem Yasser | hackerone.com/0xr3dhunt\n\n")
 
-
-
-#SHODAN API KEY
-My_API = input(bcolors['FAIL'] +"API KEY$--> "+ bcolors['ENDC']) 
+# SHODAN API KEY
+My_API = "--YOUR-API-KEY-HERE--"
 API = shodan.Shodan(My_API)
 
-#Query Input
-Query = input(bcolors['FAIL'] +"QUERY$--> "+ bcolors['ENDC'])
-
-#Query Result 1
-try:
-    Result1 = API.search_cursor(Query)   
-except shodan.APIError as error:
-    print(bcolors['FAIL'] +"!!! Error !!!"+ bcolors['ENDC'])
+# USAGE
+if len(sys.argv) != 3:
+    print(" >>>> python SHDN-HUNT.py -d <<-DOMAIN-NAME->>")
+    print(" >>>> python SHDN-HUNT.py -l <<-DOMAINS.txt->>")
+    sys.exit(0)
 
 
-#Get the Domains Name Only
-Hostnames = [ get_domain['hostnames'] for get_domain in Result1 ]
+def remove_duplicates(nested_list):
+    unique_domains = set()
+    result = []
 
-Get_Domains = []
-for host in Hostnames:
-    for string in host:
-        Get_Domains.append('.'.join(string.split('.')[-2:]))
-
-
-#PRINT DOAMINS ONLY
-print(bcolors['FAIL']+"\n==========DOMAINS========="+bcolors['ENDC'])
-print(bcolors['BOLD']+ str(set(Get_Domains)).replace('}','').replace('{','').replace("'","").replace(',','\n') +bcolors['ENDC'])
-print(bcolors['FAIL']+"==========================\n\n"+bcolors['ENDC'])
-
-
-#SAVE IPs In FILE.txt
-print(bcolors['OKGREEN']+"!!! RESULT SAVED IN RESULT.txt !!!"+bcolors['ENDC'])
-with open('Result.txt', 'w+') as file:file.write(str(set(Get_Domains)).replace(',','\n').replace("'","").replace("}","").replace("{",""))
-    
-#======================================================================================================================================
-#======================================================================================================================================
-
-'''
-#Get IPs Only & Query Result 2
-Result2 = API.search_cursor(Query,  retries=10)   
-IPs = [ ip['ip_str'] for ip in Result2 ]
-print(bcolors['BOLD']+ str(IPs).replace("[","").replace("]","").replace("'","").replace(",","\n")+bcolors['ENDC'])
+    for sublist in nested_list:
+        filtered_sublist = []
+        for domain in sublist:
+            if 'googleusercontent.com' in domain or 'amazonaws.com' in domain:
+                continue
+            if domain not in unique_domains:
+                unique_domains.add(domain)
+                filtered_sublist.append(domain)
+        result.append(filtered_sublist)
+    return result
 
 
-#Print the Count of the Result
-print(bcolors['FAIL']+"\n====================================="+bcolors['ENDC'])
-print(bcolors['FAIL']+"         TOTAL IPs RESULTS: " + str(len(IPs)) +bcolors['ENDC'])
-print(bcolors['FAIL']+"=====================================\n"+bcolors['ENDC'])
+def hunt_domain(target, output_file):
+    print(f"\n[+] Hunting: {target}")
+
+    Query = f'ssl.cert.subject.CN:"*.{target}"'
+    try:
+        Result1 = API.search_cursor(Query)
+    except shodan.APIError as error:
+        print("!!! Error with Shodan API !!!")
+        return
+
+    Hostnames = []
+    IPs = []
+
+    for get_domain in Result1:
+        hostnames = get_domain.get('hostnames', [])
+        ip = get_domain.get('ip_str', None)
+        if hostnames:
+            Hostnames.append(hostnames)
+        if ip and ':' not in ip:  # Only IPv4
+            IPs.append(ip)
+
+    final_hostnames = str(remove_duplicates(Hostnames)).replace('}', '').replace('{', '').replace('[', '').replace(']', '').replace("'", "").replace(',', '\n')
+    cleaned_hostnames = "\n".join(line.strip() for line in final_hostnames.splitlines() if line.strip())
+
+    unique_ips = sorted(set(ip.strip() for ip in IPs if ip and ':' not in ip))
+
+    # Print results
+    print("\n[+] Hostnames:\n" + cleaned_hostnames)
+    print("\n[+] IPs:")
+    for ip in unique_ips:
+        print(ip)
+
+    # Append to output file
+    with open(output_file, 'a') as file:
+        file.write(cleaned_hostnames + "\n")
+        for ip in unique_ips:
+            file.write(ip + "\n")
 
 
-#SAVE IPs In FILE.txt
-print(bcolors['OKGREEN']+"!!! RESULT SAVED IN RESULT.txt !!!"+bcolors['ENDC'])
-with open('Result.txt', 'w+') as file:file.write(str(IPs).replace(',','\n').replace("'","").replace("]","").replace("[",""))
-'''
+# Single Domain Mode
+if sys.argv[1] == "-d":
+    target = sys.argv[2]
+    output_file = f"{target}_Shdn-hunt_.txt"
+    hunt_domain(target, output_file)
+
+# List Mode
+elif sys.argv[1] == "-l":
+    filename = sys.argv[2]
+    output_file = f"{filename}_Shdn-hunt_.txt"  # All results in one file
+    try:
+        with open(filename, 'r') as f:
+            domains = [line.strip() for line in f if line.strip()]
+            for domain in domains:
+                hunt_domain(domain, output_file)
+        print(f"\n>>>> All results saved in {output_file}")
+    except FileNotFoundError:
+        print("!!! File not found:", filename)
+        sys.exit(1)
